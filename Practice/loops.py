f01f864ac41456b3abff8e33e33689bd5a72a0f1
Atharva Sawant <atharvasawant1502@gmail.com>
2026-06-03 22:14:01 +0530

    Sum of N natural numbers

--- Practice/loops.py
+++ Practice/loops.py
@@ -6,7 +6,17 @@
 # print("BYE")
 
 #Count number in reverse order
-n=int(input("From which number do you want counting to start:"))
-while n>=0:
-    print(n)
-    n-=1
+# n=int(input("From which number do you want counting to start:"))
+# while n>=0:
+#     print(n)
+#     n-=1
+
+#Sum of natural numbers
+# n=int(input("Till which number do you want Summing to continue:"))
+# i=1
+# sum=0
+# while i<=n:
+#     sum=i+sum
+#     i+=1
+
+# print(sum)
