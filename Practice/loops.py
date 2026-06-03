430a0000a81c2d2cbcd83aafef9c66a6ebb133b1
Atharva Sawant <atharvasawant1502@gmail.com>
2026-06-03 22:02:38 +0530

    Reverse Number Counting

--- Practice/loops.py
+++ Practice/loops.py
@@ -1,6 +1,12 @@
-i=1
-while i<=365:
-    print("ILY",i)
-    i+=1
+# i=1
+# while i<=365:
+#     print("ILY",i)
+#     i+=1
 
-print("BYE")  
+# print("BYE")
+
+#Count number in reverse order
+n=int(input("From which number do you want counting to start:"))
+while n>=0:
+    print(n)
+    n-=1
